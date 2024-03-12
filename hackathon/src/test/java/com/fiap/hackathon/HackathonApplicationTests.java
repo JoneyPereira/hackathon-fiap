c05afebc9d3766fd92e30a47f284df6fcb407e4a
joney.sousa <joneysousa@yahoo.com.br>
2024-03-12 06:21:18 -0300

    Criado estrutura inicial do projeto

---
+++ hackathon/src/test/java/com/fiap/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
